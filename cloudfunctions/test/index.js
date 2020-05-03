f88724d16491c07d2c30060ef6478568af0051e7
acer <wanggeli@sina.com>
2020-05-03 22:08:34 +0800

    update test function

--- cloudfunctions/test/index.js
+++ cloudfunctions/test/index.js
@@ -5,23 +5,63 @@ cloud.init()
 
 const db = require('./db.js');
 const https = require('./https.js');
-var crypto = require('crypto');
+const crypto = require('crypto');
+const cheerio = require('cheerio');
 
 // 云函数入口函数
 exports.main = async(event, context) => {
-  const wxContext = cloud.getWXContext()
-  await https.get('https://shanghaicity.openservice.kankanews.com/public/bus').then(html => {
-    //fetchBus(parseNumber(html));
-    console.log(parseNumber(html).length);
-  }).catch(error => {
-    console.log(error);
-  })
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+  const {
+    OPENID
+  } = cloud.getWXContext();
+
+  if (event.key == 'fetchNumber') {
+    var numbers = await fetchNumber();
+    return numbers;
+  } else if (event.key == 'fetchDirection') {
+    var directions = await fetchDirection("44路");
+    return directions;
+  } else if (event.key == 'fetchStop') {
+    var directions = await fetchVehicle("44路", 1, event.stop);
+    return directions;
+  } else {
+    return OPENID;
   }
+}
+
+async function test() {
+  console.time('test');
+  var count = 0;
+  var result = {};
+  var numbers = await fetchNumber();
+  numbers.length = 50;
+  var handler = setInterval(() => {
+    while (numbers.length > 0 && count < 10) {
+      count++;
+      let number = numbers.shift();
+      result[number] = {};
+      fetchDirection(number).then(direction => {
+        count--;
+        console.log(direction.number);
+        result[direction.number] = direction.directions;
+        if (count == 0) {
+          clearInterval(handler);
+          console.log(result);
+          console.timeEnd('test');
+        }
+      });
+    }
+    //console.log(handler);
+  }, 10);
+}
+
+function fetchNumber() {
+  return new Promise((resolve, reject) => {
+    https.get('https://shanghaicity.openservice.kankanews.com/public/bus').then(html => {
+      resolve(parseNumber(html));
+    }).catch(error => {
+      reject(error);
+    });
+  });
 }
 
 function parseNumber(html) {
@@ -90,6 +130,126 @@ function sortNumber(a, b, partten) {
   }
 }
 
+function fetchDirection(number) {
+  var md5 = crypto.createHash('md5');
+  var sid = md5.update(number).digest('hex');
+  var bus = {
+    number: number,
+    directions: []
+  }
+  return new Promise((resolve, reject) => {
+    fetchDirectionByType(sid).then(result => {
+      bus.directions.push(result.direction);
+      if (result.count > 1) {
+        fetchDirectionByType(sid, 1).then(result => {
+          bus.directions.push(result.direction);
+          resolve(bus);
+        }).catch(error => reject(error));
+      } else {
+        resolve(bus);
+      }
+    }).catch(error => reject(error));
+  });
+}
+
+function fetchDirectionByType(sid, type) {
+  type = type ? type : 0;
+  var result = {};
+  return new Promise((resolve, reject) => {
+    https.get("https://shanghaicity.openservice.kankanews.com/public/bus/mes/sid/" + sid + (type ? "?stoptype=" + type : "")).then(html => {
+      const $ = cheerio.load(html);
+      var directions = $(".busDirection").find(".upgoing");
+      result.count = directions.length;
+      result.direction = {
+        stations: []
+      };
+      var direction = directions.length >= type + 1 ? directions[type] : null;
+      if (direction) {
+        $(direction).find("span").each((i, element) => {
+          i == 0 ? result.direction.startStop = $(element).text() : result.direction.endStop = $(element).text();
+        });
+        $(direction).find("em").each((i, element) => {
+          i == 0 ? result.direction.startTime = $(element).text() : result.direction.endTime = $(element).text();
+        });
+      }
+      var stations = $(".stationBox").find(".name");
+      stations.each((i, element) => {
+        result.direction.stations.push($(element).text());
+      });
+      resolve(result);
+    }).catch(error => {
+      reject(error);
+    });
+  });
+}
+
+function fetchStop(type, stop, sid) {
+  return new Promise((resolve, reject) => {
+    https.form('https://shanghaicity.openservice.kankanews.com/public/bus/Getstop', {
+      stoptype: type,
+      stopid: stop + ".",
+      sid: sid
+    }).then(result => {
+      result = decodeURIComponent(result);
+      result = JSON.parse(result);
+      if (result.length) {
+        resolve(result[0]);
+      } else {
+        resolve(result);
+      }
+    }).catch(error => reject(error));
+  });
+}
+
+function fetchVehicle(number, direction, stop) {
+  var result = []
+  var md5 = crypto.createHash('md5');
+  var sid = md5.update(number).digest('hex');
+  return new Promise((resolve, reject) => {
+    fetchStop(direction, stop, sid).then(firstVehicle => {
+      firstVehicle.stop = stop;
+      result.push(firstVehicle);
+      if (firstVehicle.stopdis && stop - firstVehicle.stopdis > 1) {
+        stop = stop - firstVehicle.stopdis;
+        fetchStop(direction, stop, sid).then(secondVehicle => {
+          secondVehicle.stop = stop;
+          result.push(secondVehicle);
+          if (secondVehicle && secondVehicle.stopdis && stop - secondVehicle.stopdis > 1) {
+            stop = stop - secondVehicle.stopdis;
+            fetchStop(direction, stop, sid).then(thirdVehicle => {
+              thirdVehicle.stop = stop;
+              result.push(thirdVehicle);
+              resolve(result);
+            }).catch(error => {
+              return reject(result);
+            });
+          } else {
+            resolve(result);
+          }
+        }).catch(error => {
+          resolve(result);
+        });
+      } else {
+        resolve(result);
+      }
+    }).catch(error => {
+      reject(error);
+    });
+  });
+}
+
+// function getSid(number) {
+//   const url = 'https://shanghaicity.openservice.kankanews.com/public/bus/get';
+//   https.form(url, {
+//     idnum: number
+//   }).then(html => {
+//     var html = JSON.parse(html);
+//     console.log(number, html.sid);
+//   }).catch(error => {
+//     console.log(error)
+//   });
+// }
+
 function saveToDatabase(_id, number) {
   db.get("bus", _id).then(result => {
     console.log(result);
@@ -103,27 +263,4 @@ function saveToDatabase(_id, number) {
       console.log(number, error);
     });
   })
-}
-
-function getSid(number) {
-  const url = 'https://shanghaicity.openservice.kankanews.com/public/bus/get';
-  https.form(url, {
-    idnum: number
-  }).then(html => {
-    var html = JSON.parse(html);
-    console.log(number, html.sid);
-  }).catch(error => {
-    console.log(error)
-  });
-}
-
-function fetchBus(number) {
-  console.log(number.length);
-  for (var i = 0; i < 10; i++) {
-    var item = number[i];
-    var md5 = crypto.createHash('md5');
-    var _id = md5.update(item).digest('hex');
-    //console.log(_id, item);
-    getSid(item);
-  }
 }
